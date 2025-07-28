c4fed595e6145383d8feb72381dd766b22137de4
Rishabh Kamboj <heyrishabh13@gmail.com>
2025-07-28 19:50:13 +0530

    Min stack added

---
+++ Min-Stack.js
@@ -0,0 +1,55 @@
+var MinStack = function () {
+  this.stack = [];
+  this.min = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  this.stack.push(val);
+  if (this.min.length === 0) {
+    this.min.push(val);
+  } else if (this.min[this.min.length - 1] < val) {
+    this.min.push(this.min[this.min.length - 1]);
+  } else {
+    this.min.push(val);
+  }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  this.stack.pop();
+  this.min.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.min[this.min.length - 1];
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ 
+ */
+
+let minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+console.log(minStack.getMin()); // return -3
+minStack.pop();
+console.log(minStack.top()); // return 0
+console.log(minStack.getMin()); // return -2
