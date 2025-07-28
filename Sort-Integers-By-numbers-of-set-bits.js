0480c21d51bd80bac3dc8305a845e24688866e59
Rishabh Kamboj <heyrishabh13@gmail.com>
2025-07-28 17:45:16 +0530

    Sort Integers by number of set bits added

---
+++ Sort-Integers-By-numbers-of-set-bits.js
@@ -0,0 +1,27 @@
+var sortByBits = function (arr) {
+  arr.sort((x, y) => {
+    let countx = count(x);
+    let county = count(y);
+
+    console.log(countx, county);
+
+    if (countx === county) {
+      return x - y;
+    }
+    return countx - county;
+  });
+
+  return arr;
+};
+
+function count(num) {
+  let count = 0;
+
+  while (num > 0) {
+    count += num & 1;
+    num >>= 1;
+  }
+  return count;
+}
+
+console.log(sortByBits([0, 1, 2, 3, 4, 5, 6, 7, 8]));
